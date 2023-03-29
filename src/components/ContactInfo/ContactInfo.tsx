d02b4dd9465f4e6c562458e252de6360a6703900
Malachi Williams <malachiw95@hotmail.com>
2023-03-28 20:33:05 -0400

    change the homepage

---
+++ src/components/ContactInfo/ContactInfo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ContactInfo.css'
+
+export const ContactInfo = () => {
+    return(
+        <div className="card">
+            <h1>Contact Me</h1><br />
+            Malachi Williams <br/>
+            <a href="mailto: malachiw95@hotmail.com">malachiw95@hotmail.com</a><br/>
+            <a href="mailto: malachiw95@gmail.com">malachiw95@gmail.com</a>
+        </div>
+    )
+}
+
+export default ContactInfo
